1512805d6b1aef0684006fec5dddf8e12af0cdb5
vadim010975 <vadim010975@mail.ru>
2024-01-30 19:06:42 +0400

    completed

--- js/db.js
+++ js/db.js
@@ -2,83 +2,68 @@ const mime = require('mime-types');
 const fs = require('fs');
 const path = require('path');
 const public = path.join(path.dirname(__dirname), '/public');
+const keyFolder = path.join(path.dirname(__dirname), '/key');
 const { accessSync, constants } = require('node:fs');
 
 
 const db = {
   add(file, geolocation) {
     const subfolder = Date.now();
-
     const uploadFolder = public + '/' + subfolder;
-
     fs.mkdirSync(uploadFolder);
-
     fs.copyFileSync(file.filepath, uploadFolder + '/' + file.originalFilename);
-
     if (geolocation) {
       this.createFileGeolocation(subfolder, geolocation);
     }
-
     return this.createResponse(subfolder);
   },
 
   addMessage(text, geolocation) {
     const subfolder = Date.now();
-
     const uploadFolder = public + '/' + subfolder;
-
     fs.mkdirSync(uploadFolder);
-
     const filePath = '/' + subfolder + '/' + '%message%.txt';
-
     fs.writeFileSync(public + filePath, text);
-
     if (geolocation) {
       this.createFileGeolocation(subfolder, geolocation);
     }
-
     return this.createResponse(subfolder);
   },
 
   addLink(text, geolocation) {
     const subfolder = Date.now();
-
     const uploadFolder = public + '/' + subfolder;
-
     fs.mkdirSync(uploadFolder);
-
     const filePath = '/' + subfolder + '/' + '%link%.txt';
-
     fs.writeFileSync(public + filePath, text);
-
     if (geolocation) {
       this.createFileGeolocation(subfolder, geolocation);
     }
-
     return this.createResponse(subfolder);
   },
 
-  loadHistory(folder) {
-    const elements = fs.readdirSync(public);
+  addEncryptedMessage(text, geolocation) {
+    const subfolder = Date.now();
+    const uploadFolder = public + '/' + subfolder;
+    fs.mkdirSync(uploadFolder);
+    const filePath = '/' + subfolder + '/' + '%encrypted_message%.txt';
+    fs.writeFileSync(public + filePath, text);
+    if (geolocation) {
+      this.createFileGeolocation(subfolder, geolocation);
+    }
+    return this.createResponse(subfolder);
+  },
 
-    const folders = elements.filter(element => {
-      const stats = fs.statSync(public + '/' + element);
-      if (stats.isDirectory()) return true;
-    });
-
+  loadHistory({ type, postDate: folder, text }) {
+    const folders = this.selectFilesByType({ type, text });
     const arrayLength = folders.length;
-
     folders.sort();
-
     const data = [];
-
     let startIdx, endIdx;
-
     if (arrayLength === 0) {
-      console.log('контентная папка пустая');
+      console.log('данных нет');
       return;
     }
-
     if (arrayLength < 10) {
       startIdx = arrayLength - 1;
       endIdx = 0;
@@ -95,16 +80,12 @@ const db = {
         endIdx = arrayLength - 10;
       }
     }
-
     for (let i = startIdx; i >= endIdx; i -= 1) {
-
       const res = this.createResponse(folders[i]);
-
       if (res) {
         data.push(res);
       }
     }
-
     return {
       type: 'history',
       data,
@@ -113,19 +94,15 @@ const db = {
 
   createResponse(subfolder) {
     const files = (fs.readdirSync(public + '/' + subfolder));
-
     if (files.length === 0) {
       this.removeFolder(subfolder);
       return;
     }
-
     if (files.length === 1 && files[0] === 'geolocation.txt') {
       this.removeFolder(subfolder);
       return;
     }
-
     let file, geolocation;
-
     files.forEach(el => {
       if (el != 'geolocation.txt') {
         file = el;
@@ -133,52 +110,51 @@ const db = {
         geolocation = JSON.parse(fs.readFileSync(public + '/' + subfolder + '/' + el, 'utf8'));
       }
     });
-
     const filePath = '/' + subfolder + '/' + file;
-
     try {
       accessSync(public + filePath, constants.R_OK);
     } catch (err) {
       console.error('no access!');
       return;
     }
-
-
-    if (file === '%message%.txt' || file === '%link%.txt') {
-
+    if (file === '%message%.txt' || file === '%link%.txt' || file === '%encrypted_message%.txt') {
       const message = fs.readFileSync(public + filePath, 'utf8');
-
+      switch(file) {
+        case '%message%.txt':
+          type = 'message';
+          break;
+        case '%link%.txt':
+          type = 'link';
+          break;
+        case '%encrypted_message%.txt':
+          type = 'encrypted_message';
+      }
       return {
-        type: file === '%message%.txt' ? 'message' : 'link',
+        type,
         filePath,
         date: subfolder,
         message,
         geolocation,
       }
     }
-
     const mimetype = mime.lookup(public + filePath).split('/')[0];
-
     return {
       type: 'file',
       fileName: file,
       filePath,
       date: subfolder,
-      mimetype: mimetype,
+      mimetype,
       extname: path.extname(file),
       geolocation,
     }
   },
 
   removeFolder(subfolder) {
-
     if (!fs.readdirSync(public).includes(subfolder)) {
       console.log('папка не найдена для удаления');
       return subfolder;
     }
-
     const folderPath = public + '/' + subfolder;
-
     fs.rmSync(
       folderPath,
       {
@@ -199,77 +175,97 @@ const db = {
 
   createFileGeolocation(subfolder, geolocation) {
     const fileName = public + '/' + subfolder + '/' + 'geolocation.txt';
-
     fs.writeFileSync(fileName, geolocation);
   },
 
-  filter(search) {
-    const folders = fs.readdirSync(public);
-    let file;
-    const data = [];
-    folders.forEach(folder => {
-      const files = (fs.readdirSync(public + '/' + folder));
-      files.forEach(el => {
-        if (el === '%message%.txt' || el === '%link%.txt') {
-          file = el;
-          const textInFile = fs.readFileSync(public + '/' + folder + '/' + file, 'utf8');
-          const clean = search.trim().toLowerCase();
-          if (textInFile.toLowerCase().startsWith(clean)) {
-            const res = this.createResponse(folder);
-            if (res) {
-              data.push(res);
-            }
-          }
-        }
-      });
-    });
-    return {
-      type: 'find',
-      data,
-    };
-  },
-
   getInformation() {
-    const folders = fs.readdirSync(public);
-    let images = 0, videos = 0, audios = 0, messages = 0, links = 0, otherFiles = 0;
-    folders.forEach(folder => {
-      const files = (fs.readdirSync(public + '/' + folder));
-      files.forEach(file => {
-        if (file != 'geolocation.txt') {
-          console.log(file);
-          const mimetype = mime.lookup(public + folder + file).split('/')[0];
-          console.log(mimetype);
-          if (file === '%message%.txt') {
-            console.log('messages');
-            messages += 1;
-          } else if (file === '%link%.txt') {
-            console.log('links');
-            links += 1;
-          } else if (mimetype === 'image') {
-            console.log('images');
-            images += 1;
-          } else if (mimetype === 'audio') {
-            console.log('audios');
-            audios += 1;
-          } else if (mimetype === 'video') {
-            console.log('video');
-            videos += 1;
-          } else {
-            console.log('otherFiles');
-            otherFiles += 1;
-          }
-        }
-      });
-    });
+    const images = this.selectFilesByType({ type: 'image' }).length;
+    const videos = this.selectFilesByType({ type: 'video' }).length;
+    const audios = this.selectFilesByType({ type: 'audio' }).length;
+    const messages = this.selectFilesByType({ type: 'message' }).length;
+    const links = this.selectFilesByType({ type: 'link' }).length;
+    const encryptedMessage = this.selectFilesByType({ type: 'encrypted_message' }).length;
+    const otherFiles = this.selectFilesByType({ type: 'otherFiles' }).length;
+    const all = this.getFolders().length;
     return {
       images,
       videos,
       audios,
       messages,
       links,
+      encryptedMessage,
       otherFiles,
-      all: folders.length,
+      all,
     }
+  },
+
+  selectFilesByType({ type, text }) {
+    const folders = this.getFolders();
+    if (type === 'all') {
+      return folders;
+    }
+    return folders.filter(folder => {
+      const file = this.getContentFile(folder);
+      const mimetype = mime.lookup(public + folder + file).split('/')[0];
+      switch (type) {
+        case 'search':
+          if (file != '%message%.txt' && file != '%link%.txt') {
+            return;
+          }
+          const textInFile = fs.readFileSync(public + '/' + folder + '/' + file, 'utf8');
+          const clean = text.trim().toLowerCase();
+          return textInFile.toLowerCase().startsWith(clean);
+        case 'message':
+          return file === '%message%.txt';
+        case 'link':
+          return file === '%link%.txt';
+        case 'encrypted_message':
+          return file === '%encrypted_message%.txt';
+        case 'image':
+          return mimetype === 'image';
+        case 'audio':
+          return mimetype === 'audio';
+        case 'video':
+          return mimetype === 'video';
+        default:
+          return (
+            file != '%message%.txt' &&
+            file != '%link%.txt' &&
+            file != '%encrypted_message%.txt' &&
+            mimetype != 'image' &&
+            mimetype != 'audio' &&
+            mimetype != 'video'
+          );
+      }
+    });
+  },
+
+  getContentFile(folder) {
+    const files = (fs.readdirSync(public + '/' + folder));
+    return (files.filter(file => file != 'geolocation.txt'))[0];
+  },
+
+  getFolders() {
+    const elements = fs.readdirSync(public);
+    return elements.filter(element => {
+      const stats = fs.statSync(public + '/' + element);
+      if (stats.isDirectory()) return true;
+    });
+  },
+
+  saveKey(key) {
+    fs.writeFileSync(keyFolder + '/key.txt', key);
+    return this.getKey();
+  },
+
+  getKey() {
+    const files = (fs.readdirSync(keyFolder));
+    const file = [...files].find(el => el === 'key.txt');
+    if (!file) {
+      return { key: '' };
+    }
+    const key = fs.readFileSync(keyFolder + '/' + file, 'utf8');
+    if (key) return { key };
   }
 }
 
--- server.js
+++ server.js
@@ -20,20 +20,19 @@ app.use(koaBody({
 app.use(koaStatic(public));
 
 app.use((ctx, next) => {
-  if (ctx.request.method !== 'GET' || ctx.request.url !== '/loadHistory') {
+  if (ctx.request.method != 'GET' || ctx.request.path != '/loadHistory') {
     next();
-    
     return;
   }
-  
   let data;
-
   try {
-    data = db.loadHistory();
-
+    const { type, text } = ctx.request.query;
+    data = db.loadHistory({
+      type,
+      text
+    });
   } catch (error) {
     ctx.response.status = 500;
-    
     return;
   }
   ctx.response.body = data;
@@ -43,20 +42,14 @@ app.use((ctx, next) => {
 app.use((ctx, next) => {
   if (ctx.request.method !== 'POST' || ctx.request.url !== '/loadHistory/next/') {
     next();
-    
     return;
   }
-
   let data;
-
   try {
-    const text = ctx.request.body;
-
-    data = db.loadHistory(text);
-
+    const { type, postDate, text } = ctx.request.body;
+    data = db.loadHistory({ type, postDate, text });
   } catch (error) {
     ctx.response.status = 500;
-    
     return;
   }
   ctx.response.body = data;
@@ -66,50 +59,34 @@ app.use((ctx, next) => {
 app.use((ctx, next) => {
   if (ctx.request.method !== 'POST' || ctx.request.url !== '/upload') {
     next();
-    
     return;
   }
-
   let fileData;
-
   try {
     const { file } = ctx.request.files;
-
     const { geolocation } = ctx.request.body;
-
     fileData = db.add(file, geolocation);
-
   } catch (error) {
     console.log(error);
-
     ctx.response.status = 500;
-    
     return;
   }
-
   ctx.response.body = fileData;
 });
 
 app.use((ctx, next) => {
   if (ctx.request.method !== 'POST' || ctx.request.url !== '/message') {
     next();
-    
     return;
   }
-
   let fileData;
-
   try {
     const { text, geolocation } = ctx.request.body;
-
     fileData = db.addMessage(text, geolocation);
-
   } catch (error) {
     ctx.response.status = 500;
-    
     return;
   }
-
   ctx.response.body = fileData;
 });
 
@@ -117,92 +94,94 @@ app.use((ctx, next) => {
 app.use((ctx, next) => {
   if (ctx.request.method !== 'POST' || ctx.request.url !== '/link') {
     next();
-    
     return;
   }
-
   let fileData;
-
   try {
     const { text, geolocation } = ctx.request.body;
-
     fileData = db.addLink(text, geolocation);
-
   } catch (error) {
-    ctx.response.status = 500;
-    
+    ctx.response.status = 500; 
     return;
   }
-
   ctx.response.body = fileData;
 });
 
 app.use((ctx, next) => {
-  if (ctx.request.method !== 'POST' || ctx.request.url !== '/remove') {
+  if (ctx.request.method !== 'POST' || ctx.request.url !== '/encrypted_message') {
     next();
-    
     return;
   }
-
-  let data;
-
+  let fileData;
   try {
-    const text = ctx.request.body;
-
-    data = db.removeFolder(text);
-
+    const { text, geolocation } = ctx.request.body;
+    fileData = db.addEncryptedMessage(text, geolocation);
   } catch (error) {
-    ctx.response.status = 500;
-    
+    ctx.response.status = 500; 
     return;
   }
+  ctx.response.body = fileData;
+});
 
+app.use((ctx, next) => {
+  if (ctx.request.method !== 'POST' || ctx.request.url !== '/key') {
+    next();
+    return;
+  }
+  let data;
+  try {
+    const { key } = ctx.request.body;
+    data = db.saveKey(key);
+  } catch (error) {
+    ctx.response.status = 500; 
+    return;
+  }
   ctx.response.body = data;
 });
 
 app.use((ctx, next) => {
-  if (ctx.request.method !== 'GET' || ctx.request.path !== '/find') {
+  if (ctx.request.method !== 'GET' || ctx.request.url !== '/key') {
     next();
-    
     return;
   }
-
   let data;
+  // try {
+    data = db.getKey();
+  // } catch (error) {
+  //   ctx.response.status = 500; 
+  //   return;
+  // }
+  ctx.response.body = data;
+});
 
+app.use((ctx, next) => {
+  if (ctx.request.method != 'POST' || ctx.request.url != '/remove') {
+    next();
+    return;
+  }
+  let data;
   try {
-
-    const text = ctx.request.query.text;
-
-    data = db.filter(text);
-
+    const text = ctx.request.body;
+    data = db.removeFolder(text);
   } catch (error) {
     ctx.response.status = 500;
-    
     return;
   }
-
   ctx.response.body = data;
 });
 
 app.use((ctx, next) => {
   if (ctx.request.method !== 'GET' || ctx.request.url !== '/getinformation') {
     next();
-    
     return;
   }
-
   let data;
-
   try {
-
     data = db.getInformation();
-
   } catch (error) {
     ctx.response.status = 500;
-    
     return;
   }
-
   ctx.response.body = data;
 });
 
@@ -213,7 +192,6 @@ const port = process.env.PORT || 7070;
 server.listen(port, (err) => {
   if (err) {
     console.log(err);
-
     return;
   }
 
